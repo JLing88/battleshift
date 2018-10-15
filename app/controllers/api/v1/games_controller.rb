e638384e9e640c2b8c44d92b5c15a92bbf6f68e3
andrewetobin <andrewetobin@gmail.com>
2018-10-15 13:32:46 -0600

    now on line 20

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -10,6 +10,22 @@ module Api
       end
 
       def create
+        player_1_board = Board.new(4)
+        player_2_board = Board.new(4)
+        sm_ship = Ship.new(2)
+        md_ship = Ship.new(3)
+        current_turn = "player_1"
+
+
+        game_attributes = {
+          player_1_board: player_1_board,
+          player_2_board: player_2_board,
+          player_1_turns: 0,
+          player_2_turns: 0,
+          current_turn: "player_1"
+       }
+       game = Game.create(game_attributes)
+       render json: game 
       end
     end
   end
