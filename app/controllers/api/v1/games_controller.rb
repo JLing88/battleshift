4ba1b6ca678630a309b9dfef9da8c6ed00471732
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-10-17 16:41:05 -0600

    Failing on line 71 of harness

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -13,14 +13,16 @@ module Api
         player_1_board = Board.new(4)
         player_2_board = Board.new(4)
         current_turn = "player_1"
-
-
+        player_1 = User.find_by(api_key: request.headers["HTTP_X_API_KEY"])
+        player_2 = User.find_by(email: params[:opponent_email])
         game_attributes = {
           player_1_board: player_1_board,
           player_2_board: player_2_board,
           player_1_turns: 0,
           player_2_turns: 0,
-          current_turn: "player_1"
+          player_1_id: player_1.id,
+          player_2_id: player_2.id,
+          current_turn: current_turn
        }
        game = Game.create(game_attributes)
        render json: game
